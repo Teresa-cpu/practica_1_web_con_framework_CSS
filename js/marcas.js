7d3412158d984e955176567f62c5f25fed216bdf
AliciaSanchez21 <aliciamicaela.sanchez@alumnos.udit.es>
2025-11-01 13:05:49 +0100

    refinar comentarios en marcas
    comentado la estructura y quitado algun comentario que no necesitaba

--- js/marcas.js
+++ js/marcas.js
@@ -16,23 +16,8 @@ $(window).on('scroll', function() {
   });
   
 
-  
 
-// CON RETARDO 1 POR 1
-//   $(window).on('scroll', function() {
-//     $('.card').each(function(i) {
-//       const cardTop = $(this).offset().top;
-//       const scrollTop = $(window).scrollTop();
-//       const windowHeight = $(window).height();
-  
-//       if (cardTop < scrollTop + windowHeight - 100) {
-//         $(this).css('transition-delay', `${i * 0.1}s`).addClass('visible');
-//       } else {
-//         $(this).removeClass('visible');
-//       }
-//     });
-//   });
-  
+//   ANIMACION TITULOS HEADER 
 
 document.addEventListener('DOMContentLoaded', function() {
     if (typeof gsap === 'undefined') {
